0a92e690817d41df1c639c6234cdde4644d52e48
Sanka Tillakarathne <52779241+SankaLakshan@users.noreply.github.com>
2021-09-22 00:51:38 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,14 +5,16 @@
 #-----------------------------------
 # wrote by my self - Sanka
 #-----------------------------------
-# 
+# cccccc
 #-----------------------------------
 
 clear
 
 echo staring script 2
 
-
+echo staring script 2
+echo staring script 2
+echo staring script 2
 /usr/apps/Tomcat/apache-tomcat-9.0.27/bin/startup.sh
 
 echo end of script 2
